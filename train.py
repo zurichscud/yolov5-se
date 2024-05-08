aa9c013a9a07b149aab9dd14701ea9e8a37f8c03
zurichscud <zurichscud@outlook.com>
2024-05-09 04:09:45 +0800

    update

--- train.py
+++ train.py
@@ -517,7 +517,7 @@ def parse_opt(known=False):
     """Parses command-line arguments for YOLOv5 training, validation, and testing."""
     parser = argparse.ArgumentParser()
     parser.add_argument("--weights", type=str, default=ROOT / "yolov5s.pt", help="initial weights path")
-    parser.add_argument("--cfg", type=str, default=ROOT / "models/yolov5s-CBAM.yaml", help="model.yaml path")
+    parser.add_argument("--cfg", type=str, default=ROOT / "models/yolov5s.yaml", help="model.yaml path")
     parser.add_argument("--data", type=str, default=ROOT / "data/coco128.yaml", help="dataset.yaml path")
     parser.add_argument("--hyp", type=str, default=ROOT / "data/hyps/hyp.scratch-low.yaml", help="hyperparameters path")
     parser.add_argument("--epochs", type=int, default=100, help="total training epochs")
